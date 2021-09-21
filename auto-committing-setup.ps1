b2a1cc47ca2ae15ccd4e0ef6000d684612a0c25a
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-09-21 17:50:59 -0400

    auto committed from auto-committing-steup.ps1!

--- auto-committing-setup.ps1
+++ auto-committing-setup.ps1
@@ -3,12 +3,12 @@ $CommitMessage='auto committed from auto-committing-steup.ps1!'
 $PracticeRepoDir='E:\Educational\LC\PowerShell'
 
 #4. Clone your forked repo into a directory at the $PracticeRepoDir path
-git clone "https://github.com/$GitHubUsername/powershell-practice.git" "$PracticeRepoDir"\powershell-github-practice
+git clone "https://github.com/$GitHubUsername/powershell-practice.git" "$PracticeRepoDir"
 
 #Write-Output "PSCommandPath is: $PSCommandPath"
 
 #5. Copy the auto-committing-setup.ps1 script (by its absolute path) into the cloned repo directory
-Copy-Item $PracticeRepoDir\auto-committing-setup.ps1 $PracticeRepoDir\powershell-github-practice\auto-committing-setup.ps1
+Copy-Item $PracticeRepoDir\auto-committing-setup.ps1 $PracticeRepoDir\auto-committing-setup.ps1
 
 #6. Change into the cloned directory ($PracticeRepoDir)
 Set-Location $PracticeRepoDir\powershell-github-practice
